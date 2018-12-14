8405d23f911884c31a61f4682364b00b34804027
Oliver <oliver.grant@gmail.com>
2018-12-14 12:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var letsmakemusic=[]
+  var str1,str2,str3
+  for (i=0;i<musicians.length;i++) {
+    str1=musicians[i]
+    str2=instruments[i]
+    str3=str1.concat(" plays ",str2)
+    letsmakemusic[i]=str3
+  }
+  return letsmakemusic
+}
+
+function johnLennonFacts(facts) {
+  var str 
+  var i=0
+  while (i<facts.length) {
+    str=facts[i]
+    str=str.concat("!!!")
+    facts[i]=str
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var array=[]
+  do {
+    array[number]="I love the Beatles!"
+    number++
+  } while ([number<15])
+  return array
+}
