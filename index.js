b3eef340344a8760a902cb4f0d36635c2ad187f6
Oliver <oliver.grant@gmail.com>
2018-12-14 11:25:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay (musicians,instruments) {
+  var letsmakemusic=[]
+  var str1,str2,str3
+  for (i=-1;i<musicians.length;i++) {
+    str1=musicians[i]
+    str2=instruments[i]
+    str3=str1.concat(" plays ",str2)
+    letsmakemusic[i]=str3
+  }
+  return letsmakemusic
+}
